1e691cd9b705feb53505b73dbf7a1056ad2b58a4
ramashishmaurya <ramashishmaurya738@gmail.com>
2026-09-21 11:53:55 +0530

    pattern

--- main.py
+++ main.py
@@ -21,9 +21,35 @@
 # print(groupsanagram(n))
 
 
-def sumnumber():
-    return 10 
+# def sumnumber():
+#     return 10 
 
 
+# character print okay 
+
+# n = 4 
+# for i in range(1 , n):
+#     for j in range(1 , i+1):
+#         print(j , end=" ")
+#     print( )
+
+# n  =5 
+# for i in range(n):
+#     for j in range(i +1):
+#         print(chr(65 + j) , end=" ")
+#     print( )
+    # char is functio that convert speciafic number to the character valeus right
+
+
+number = [1, 2, 3, 4]
+
+def function(number):
+    return number ** 2
+
+n = list(map(function, number))
+
+abc = [i**2 for i in range(1,9)]
+
+print(abc)
 
 
--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,93 @@
-from main import sumnumber
+from main import sumnumber 
 
 
 def testdata():
     assert sumnumber() == 10
+
+
+nums = [3 ,2,3 ,3]
+
+def functiomajorityelement(nums):
+
+    result = {}
+
+    for  i in nums:
+
+        if i not in result:
+            result[i] = 1 
+        else:
+            result[i]+= 1 
+
+    for n in result:
+        if result[n] > len(nums) / 2  :
+            return n
+        
+
+abc = functiomajorityelement(nums)
+
+print(abc) 
+ 
+
+nrows = 5 
+ncols = 4 
+
+for i in range(1 , nrows):
+    for space in range(1 , nrows - i ):
+        print(" " , end=" ")
+
+    for start in range(i):
+        print("*" , end=" ")
+    
+    print()
+
+nrow =5
+for i in range(1 , nrows):
+    for space in range(i):
+        print(" " , end=" ")
+    
+    for start in range(nrow- i):
+        print("*" , end=" ")
+    
+    print( ) 
+
+
+sumvalues = 1 
+nums = 6
+for i in range(1 , nums):
+    for j in range(i):
+        print(sumvalues , end=" ")
+        sumvalues+=1 
+    print()
+
+
+
+n = 5
+
+# Upper half
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end=" ")
+
+    for j in range(2 * (n - i)):
+        print(" ", end=" ")
+
+    for j in range(i):
+        print("*", end=" ")
+
+    print()
+
+# Lower half
+for i in range(n - 1, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+
+    for j in range(2 * (n - i)):
+        print(" ", end=" ")
+
+    for j in range(i):
+        print("*", end=" ")
+
+    print()
+
 
 
